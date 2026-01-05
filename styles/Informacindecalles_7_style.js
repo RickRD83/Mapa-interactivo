ffbcb19aefbc8237621fe2847931224767d1227d
RickRD83 <ricardofigueroa8327@gmail.com>
2026-01-04 21:05:03 -0500

    Add files via upload

---
+++ styles/Informacindecalles_7_style.js
@@ -0,0 +1,57 @@
+var size = 0;
+var placement = 'point';
+function categories_Informacindecalles_7(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Alto':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(229,21,22,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Medio':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(225,179,36,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Bajo':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(24,177,51,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Informacindecalles_7 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("nivel_ries");
+    var labelFont = "14.3px \'Arial Narrow\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 2.0999999999999996;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if (feature.get("nombre_via") !== null) {
+        labelText = String(feature.get("nombre_via"));
+    }
+    
+    var style = categories_Informacindecalles_7(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
